95d03e014bd75d4726efaab57c70c8b31598f73f
timgent <tim.gent@gmail.com>
2021-02-19 22:22:48 +0000

    Add check to compare schemas between 2 datasets

--- src/main/scala/com/github/timgent/dataflare/checks/ArbDualDsCheck.scala
+++ src/main/scala/com/github/timgent/dataflare/checks/ArbDualDsCheck.scala
@@ -46,4 +46,29 @@ object ArbDualDsCheck {
       }
     }
   }
+
+  /**
+    * Checks if schemas of 2 datasets exactly match
+    */
+  val dsSchemasMatch = ArbDualDsCheck("Dataset schemas match") { dsPair =>
+    val df1Fields = dsPair.ds.schema.fields.sortBy(_.name).toList
+    val df2Fields = dsPair.dsToCompare.schema.fields.sortBy(_.name).toList
+    if (df1Fields == df2Fields) {
+      RawCheckResult(CheckStatus.Success, "Dataset schemas successfully matched")
+    } else {
+      val colsInDf1Not2 = df1Fields.map(_.name).diff(df2Fields.map(_.name))
+      val colsInDf2Not1 = df2Fields.map(_.name).diff(df1Fields.map(_.name))
+      val commonCols = df1Fields.map(_.name).intersect(df2Fields.map(_.name))
+      val df1CommonFields = df1Fields.filter(field => commonCols.contains(field.name))
+      val df2CommonFields = df2Fields.filter(field => commonCols.contains(field.name))
+      val zippedCommonFields = df1CommonFields.zip(df2CommonFields)
+      val fieldsWithTypeErrors = zippedCommonFields.filter { case (df1Field, df2Field) => df1Field.dataType != df2Field.dataType }
+      val errors =
+        colsInDf1Not2.map(c => s"Column $c was present only in ds") ++
+          colsInDf2Not1.map(c => s"Column $c was present only in dsToCompare") ++
+          fieldsWithTypeErrors.map { case (f1, f2) => s"Column ${f1.name} types did not match (${f1.dataType} vs ${f2.dataType})" }
+      val errMsg = "Dataset schemas did not match: " + errors.mkString(". ")
+      RawCheckResult(CheckStatus.Error, errMsg)
+    }
+  }
 }
