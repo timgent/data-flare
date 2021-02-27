6dd3ff7e7bade261c1056a67cf280b010226fe60
Tim Gent <tim.gent@gmail.com>
2021-02-27 08:55:30 +0000

    Add datasets are equal check (ordered and unordered) (#109)
    * Datasets are equal check implemented
    * Add check for comparing 2 dfs regardless of order

--- src/main/scala/com/github/timgent/dataflare/checks/ArbDualDsCheck.scala
+++ src/main/scala/com/github/timgent/dataflare/checks/ArbDualDsCheck.scala
@@ -1,10 +1,14 @@
 package com.github.timgent.dataflare.checks
 
+import cats.Foldable
 import com.github.timgent.dataflare.FlareError.ArbCheckError
 import com.github.timgent.dataflare.checks.CheckDescription.SimpleCheckDescription
 import com.github.timgent.dataflare.checks.QCCheck.DualDsQCCheck
 import com.github.timgent.dataflare.checkssuite.DescribedDsPair
-import org.apache.spark.sql.Dataset
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.functions.{col, count, lit}
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.{DataFrame, Dataset, Row}
 
 import scala.util.{Failure, Success, Try}
 
@@ -47,14 +51,133 @@ object ArbDualDsCheck {
     }
   }
 
+  private def zipWithIndex[T](rdd: RDD[T]): RDD[(Long, T)] = rdd.zipWithIndex.map { case (row, i) => (i, row) }
+
   /**
-    * Checks if schemas of 2 datasets exactly match
+    * Check that dfs match exactly regardless of content
     */
-  val dsSchemasMatch = ArbDualDsCheck("Dataset schemas match") { dsPair =>
+  val dfsMatchUnordered: ArbDualDsCheck = ArbDualDsCheck("Unordered dataset content matches") { dsPair =>
+    val schemaMatchCheckResult = doSchemasMatch(dsPair)
+    schemaMatchCheckResult match {
+      case Left(errMsg) => RawCheckResult(CheckStatus.Error, errMsg)
+      case Right(schema) =>
+        val cols: Array[String] = schema.fields.map(f => f.name)
+        val renamedCols = cols.map(c => s"${c}_to_compare")
+        val colsWithCount: Array[String] = "count" +: cols
+        val renamedColsWithCount = "count_to_compare" +: renamedCols
+        val dsRowCounts = dsPair.ds.groupBy(cols.map(col): _*).agg(count("*").as("count"))
+        def renameCols(ds: Dataset[_]): DataFrame = {
+          cols.zip(renamedCols).foldLeft(ds.toDF) { case (ds, (c, cRenamed)) => ds.withColumnRenamed(c, cRenamed) }
+        }
+        val dsToCompareRowCounts =
+          renameCols(dsPair.dsToCompare).groupBy(renamedCols.map(col): _*).agg(count("*").as("count_to_compare"))
+        val joinExpr = colsWithCount.zip(renamedColsWithCount).foldLeft(lit(true)) {
+          case (joinExpr, (c, cRenamed)) => joinExpr && col(c) === col(cRenamed)
+        }
+        val joined = dsRowCounts.join(dsToCompareRowCounts, joinExpr, "fullouter")
+        val diffRows = joined.filter(col(cols.head).isNull || col(renamedCols.head).isNull)
+        val firstBadRow = diffRows.limit(1).collect.toList.headOption
+        firstBadRow match {
+          case Some(badRow) =>
+            if (Option(badRow.getAs[Long]("count")).isEmpty) {
+              val mismatchedRowInDsToCompare: Map[String, Any] = badRow.getValuesMap(renamedCols)
+              val mismatchedRowWithOriginalColNames = mismatchedRowInDsToCompare.map {
+                case (c, v) => (c.substring(0, c.length - "_to_compare".length), v)
+              }
+              val mismatchedRowCount = badRow.getAs[Long]("count_to_compare")
+              RawCheckResult(
+                CheckStatus.Error,
+                s"Datasets did not match: First mismatch was in dsToCompare where ${mismatchedRowCount} row(s) had " +
+                  s"content: ${prettyValuesMap(mismatchedRowWithOriginalColNames)}"
+              )
+            } else {
+              val mismatchedRowInDs = badRow.getValuesMap(cols)
+              val mismatchedRowCount = badRow.getAs[Long]("count")
+              RawCheckResult(
+                CheckStatus.Error,
+                s"Datasets did not match: First mismatch was in ds where ${mismatchedRowCount} row(s) had " +
+                  s"content: ${prettyValuesMap(mismatchedRowInDs)}"
+              )
+            }
+          case None => RawCheckResult(CheckStatus.Success, "Datasets matched")
+        }
+    }
+  }
+
+  /**
+    * Checks that dfs match exactly and are in the same order
+    */
+  val dfsMatchOrdered: ArbDualDsCheck = ArbDualDsCheck("Ordered dataset content matches") { dsPair =>
+    import cats.implicits._
+    val schemaMatchCheckResult = doSchemasMatch(dsPair)
+    schemaMatchCheckResult match {
+      case Left(errMsg) => RawCheckResult(CheckStatus.Error, errMsg)
+      case Right(schema) =>
+        val dsRdd = dsPair.ds.toDF.rdd
+        val dsToCompareRdd = dsPair.dsToCompare.toDF.rdd
+        val dsWithIndex: RDD[(Long, Row)] = zipWithIndex(dsRdd)
+        val dsToCompareWithIndex: RDD[(Long, Row)] = zipWithIndex(dsToCompareRdd)
+        val joined: RDD[(Option[Row], Option[Row])] = dsWithIndex.fullOuterJoin(dsToCompareWithIndex).map(_._2)
+
+        val sampleMismatchedRows: RDD[(Option[Row], Option[Row])] = joined.mapPartitions { it =>
+          val allRowsInPartitionMatch: Either[(Option[Row], Option[Row]), Unit] = Foldable[Stream].foldM(it.toStream, ()) {
+            case (_, (Some(rdd1Row), Some(rdd2Row))) =>
+              if (rdd1Row != rdd2Row) {
+                Left((Some(rdd1Row), Some(rdd2Row)))
+              } else {
+                Right(())
+              }
+            case (_, (Some(rdd1Row), None)) => Left(Some(rdd1Row), None)
+            case (_, (None, Some(rdd2Row))) => Left(None, Some(rdd2Row))
+          }
+          allRowsInPartitionMatch match {
+            case Left(firstMismatchedRow) => Iterator(firstMismatchedRow)
+            case Right(_)                 => Iterator.empty
+          }
+        }
+        sampleMismatchedRows.cache
+        if (sampleMismatchedRows.isEmpty) {
+          sampleMismatchedRows.unpersist(false)
+          RawCheckResult(CheckStatus.Success, "Datasets are identical")
+        } else {
+          val sampleMismatchedRowPair = sampleMismatchedRows.first
+          sampleMismatchedRows.unpersist(false)
+          sampleMismatchedRowPair match {
+            case (Some(dsRow), Some(dsToCompareRow)) =>
+              val prettyDsRow = prettyRow(dsRow, schema)
+              val prettyDsToCompareRow = prettyRow(dsToCompareRow, schema)
+              RawCheckResult(
+                CheckStatus.Error,
+                s"Datasets encountered first mismatch at ds row: $prettyDsRow. dsToCompareRow: $prettyDsToCompareRow"
+              )
+            case (Some(dsRow), None) =>
+              RawCheckResult(CheckStatus.Error, s"ds had extras rows, first extra row found: ${prettyRow(dsRow, schema)}")
+            case (None, Some(dsToCompareRow)) =>
+              RawCheckResult(CheckStatus.Error, s"dsToCompare had extras rows, first extra row found: ${prettyRow(dsToCompareRow, schema)}")
+            case (None, None) =>
+              throw new RuntimeException(
+                "Please report this issue to the library authors, this should never happen!"
+              )
+          }
+        }
+    }
+  }
+
+  private def prettyRow(row: Row, schema: StructType): String = {
+    val rowMap: Map[String, Any] = row.getValuesMap(schema.fields.map(_.name))
+    prettyValuesMap(rowMap)
+  }
+
+  private def prettyValuesMap(rowMap: Map[String, Any]) = {
+    val fieldStrings = rowMap.map { case (field, value) => s"$field=$value" }
+    fieldStrings.mkString(", ")
+  }
+
+  private def doSchemasMatch(dsPair: DatasetPair): Either[String, StructType] = {
     val df1Fields = dsPair.ds.schema.fields.sortBy(_.name).toList
     val df2Fields = dsPair.dsToCompare.schema.fields.sortBy(_.name).toList
     if (df1Fields == df2Fields) {
-      RawCheckResult(CheckStatus.Success, "Dataset schemas successfully matched")
+      Right(dsPair.ds.schema)
     } else {
       val colsInDf1Not2 = df1Fields.map(_.name).diff(df2Fields.map(_.name))
       val colsInDf2Not1 = df2Fields.map(_.name).diff(df1Fields.map(_.name))
@@ -68,7 +191,17 @@ object ArbDualDsCheck {
           colsInDf2Not1.map(c => s"Column $c was present only in dsToCompare") ++
           fieldsWithTypeErrors.map { case (f1, f2) => s"Column ${f1.name} types did not match (${f1.dataType} vs ${f2.dataType})" }
       val errMsg = "Dataset schemas did not match: " + errors.mkString(". ")
-      RawCheckResult(CheckStatus.Error, errMsg)
+      Left(errMsg)
+    }
+  }
+
+  /**
+    * Checks if schemas of 2 datasets exactly match
+    */
+  val dsSchemasMatch = ArbDualDsCheck("Dataset schemas match") { dsPair =>
+    doSchemasMatch(dsPair) match {
+      case Left(errMsg) => RawCheckResult(CheckStatus.Error, errMsg)
+      case Right(_)     => RawCheckResult(CheckStatus.Success, "Dataset schemas successfully matched")
     }
   }
 }
--- src/test/scala/com/github/timgent/dataflare/checks/ArbDualDsCheckTest.scala
+++ src/test/scala/com/github/timgent/dataflare/checks/ArbDualDsCheckTest.scala
@@ -13,20 +13,25 @@ case class SampleData2(a: String, b: String, c: Int, e: String)
 
 class ArbDualDsCheckTest extends AnyWordSpec with DatasetSuiteBase with Matchers {
   import spark.implicits._
+
+  private def assertCheckReturnsSchemaErrorForSchemaMismatch(check: ArbDualDsCheck, checkDescription: String) = {
+    val df1 = DescribedDs(spark.emptyDataset[SampleData1], "df1")
+    val df2 = DescribedDs(spark.emptyDataset[SampleData2], "df2")
+    val dsPair = DescribedDsPair(df1, df2)
+    val checkResult = check.applyCheck(dsPair)
+    checkResult.qcType shouldBe QcType.ArbDualDsCheck
+    checkResult.status shouldBe CheckStatus.Error
+    checkResult.resultDescription should (include("Dataset schemas did not match") and
+      include("Column c types did not match (StringType vs IntegerType)") and
+      include("Column d was present only in ds") and
+      include("Column e was present only in dsToCompare"))
+    checkResult.checkDescription shouldBe SimpleCheckDescription(checkDescription)
+    checkResult.datasourceDescription shouldBe Some(DualDsDescription("df1", "df2"))
+  }
+
   "ArbDualDsCheck.dsSchemasMatch" should {
     "return an error if the schemas do not match, including details of the mismatch" in {
-      val df1 = DescribedDs(spark.emptyDataset[SampleData1], "df1")
-      val df2 = DescribedDs(spark.emptyDataset[SampleData2], "df2")
-      val dsPair = DescribedDsPair(df1, df2)
-      val checkResult = ArbDualDsCheck.dsSchemasMatch.applyCheck(dsPair)
-      checkResult.qcType shouldBe QcType.ArbDualDsCheck
-      checkResult.status shouldBe CheckStatus.Error
-      checkResult.resultDescription should (include("Dataset schemas did not match") and
-        include("Column c types did not match (StringType vs IntegerType)") and
-        include("Column d was present only in ds") and
-        include("Column e was present only in dsToCompare"))
-      checkResult.checkDescription shouldBe SimpleCheckDescription("Dataset schemas match")
-      checkResult.datasourceDescription shouldBe Some(DualDsDescription("df1", "df2"))
+      assertCheckReturnsSchemaErrorForSchemaMismatch(ArbDualDsCheck.dsSchemasMatch, "Dataset schemas match")
     }
 
     "return success if the dataset schemas match exactly" in {
@@ -38,6 +43,149 @@ class ArbDualDsCheckTest extends AnyWordSpec with DatasetSuiteBase with Matchers
       checkResult.status shouldBe CheckStatus.Success
       checkResult.resultDescription shouldBe "Dataset schemas successfully matched"
       checkResult.checkDescription shouldBe SimpleCheckDescription("Dataset schemas match")
+      checkResult.datasourceDescription shouldBe Some(DualDsDescription("df1", "df2"))
+    }
+  }
+
+  "ArbDualDsCheck.dfsMatchOrdered" should {
+    "return an error if the schemas of the datasets do not match, including details of the mismatch" in {
+      assertCheckReturnsSchemaErrorForSchemaMismatch(ArbDualDsCheck.dfsMatchOrdered, "Ordered dataset content matches")
+    }
+
+    "return an error if the rows in the dataset do not match, including details of the mismatch" in {
+      val df1 = DescribedDs(
+        List(
+          SampleData1("same", "same", "same", 1),
+          SampleData1("df1only", "same", "same", 2)
+        ).toDS,
+        "df1"
+      )
+      val df2 = DescribedDs(
+        List(
+          SampleData1("same", "same", "same", 1),
+          SampleData1("df2only", "same", "same", 2)
+        ).toDS,
+        "df2"
+      )
+      val dsPair = DescribedDsPair(df1, df2)
+      val checkResult = ArbDualDsCheck.dfsMatchOrdered.applyCheck(dsPair)
+      checkResult.qcType shouldBe QcType.ArbDualDsCheck
+      checkResult.status shouldBe CheckStatus.Error
+      checkResult.resultDescription shouldBe "Datasets encountered first mismatch at ds row: a=df1only, " +
+        "b=same, c=same, d=2. dsToCompareRow: a=df2only, b=same, c=same, d=2"
+      checkResult.checkDescription shouldBe SimpleCheckDescription("Ordered dataset content matches")
+      checkResult.datasourceDescription shouldBe Some(DualDsDescription("df1", "df2"))
+    }
+
+    "return an error if one dataset has a duplicated row that the other dataset does not" in {
+      val df1 = DescribedDs(List(SampleData1("same", "same", "same", 1), SampleData1("same", "same", "same", 1)).toDS, "df1")
+      val df2 = DescribedDs(List(SampleData1("same", "same", "same", 1)).toDS, "df2")
+      val dsPair = DescribedDsPair(df1, df2)
+      val checkResult = ArbDualDsCheck.dfsMatchOrdered.applyCheck(dsPair)
+      checkResult.qcType shouldBe QcType.ArbDualDsCheck
+      checkResult.status shouldBe CheckStatus.Error
+      checkResult.resultDescription shouldBe s"ds had extras rows, first extra row found: a=same, " +
+        "b=same, c=same, d=1"
+      checkResult.checkDescription shouldBe SimpleCheckDescription("Ordered dataset content matches")
+      checkResult.datasourceDescription shouldBe Some(DualDsDescription("df1", "df2"))
+    }
+
+    "return an error if the datasets have the same data in different orders" in {
+      val df1 = DescribedDs(List(SampleData1("same", "same", "same", 1), SampleData1("same", "same", "same", 2)).toDS, "df1")
+      val df2 = DescribedDs(List(SampleData1("same", "same", "same", 2), SampleData1("same", "same", "same", 1)).toDS, "df2")
+      val dsPair = DescribedDsPair(df1, df2)
+      val checkResult = ArbDualDsCheck.dfsMatchOrdered.applyCheck(dsPair)
+      checkResult.qcType shouldBe QcType.ArbDualDsCheck
+      checkResult.status shouldBe CheckStatus.Error
+      checkResult.resultDescription shouldBe s"Datasets encountered first mismatch at ds row: a=same, b=same, " +
+        s"c=same, d=1. dsToCompareRow: a=same, b=same, c=same, d=2"
+      checkResult.checkDescription shouldBe SimpleCheckDescription("Ordered dataset content matches")
+      checkResult.datasourceDescription shouldBe Some(DualDsDescription("df1", "df2"))
+    }
+
+    "return success if the rows in the datasets match exactly (regardless of order)" in {
+      val df1 = DescribedDs(List(SampleData1("same", "same", "same", 1), SampleData1("same", "same", "same", 2)).toDS, "df1")
+      val df2 = DescribedDs(
+        List(
+          SampleData1("same", "same", "same", 1),
+          SampleData1("same", "same", "same", 2)
+        ).toDS,
+        "df2"
+      )
+      val dsPair = DescribedDsPair(df1, df2)
+      val checkResult = ArbDualDsCheck.dfsMatchOrdered.applyCheck(dsPair)
+      checkResult.qcType shouldBe QcType.ArbDualDsCheck
+      checkResult.status shouldBe CheckStatus.Success
+      checkResult.resultDescription should include("Datasets are identical")
+      checkResult.checkDescription shouldBe SimpleCheckDescription("Ordered dataset content matches")
+      checkResult.datasourceDescription shouldBe Some(DualDsDescription("df1", "df2"))
+    }
+
+    "return success if the datasets have the same schema but no rows" in {
+      val df1 = DescribedDs(spark.emptyDataset[SampleData1], "df1")
+      val df2 = DescribedDs(spark.emptyDataset[SampleData1], "df2")
+      val dsPair = DescribedDsPair(df1, df2)
+      val checkResult = ArbDualDsCheck.dfsMatchOrdered.applyCheck(dsPair)
+      checkResult.qcType shouldBe QcType.ArbDualDsCheck
+      checkResult.status shouldBe CheckStatus.Success
+      checkResult.resultDescription should include("Datasets are identical")
+      checkResult.checkDescription shouldBe SimpleCheckDescription("Ordered dataset content matches")
+      checkResult.datasourceDescription shouldBe Some(DualDsDescription("df1", "df2"))
+    }
+  }
+
+  "ArbDualDsCheck.dfsMatchUnordered" should {
+    "return an error if the schemas of the datasets do not match, including details of the mismatch" in {
+      assertCheckReturnsSchemaErrorForSchemaMismatch(ArbDualDsCheck.dfsMatchUnordered, "Unordered dataset content matches")
+    }
+
+    "return an error if one dataset has duplicate rows" in {
+      val df1 = DescribedDs(List(SampleData1("dupe", "dupe", "dupe", 1), SampleData1("dupe", "dupe", "dupe", 1)).toDS, "df1")
+      val df2 = DescribedDs(List(SampleData1("dupe", "dupe", "dupe", 1)).toDS, "df2")
+      val dsPair = DescribedDsPair(df1, df2)
+      val checkResult = ArbDualDsCheck.dfsMatchUnordered.applyCheck(dsPair)
+      checkResult.qcType shouldBe QcType.ArbDualDsCheck
+      checkResult.status shouldBe CheckStatus.Error
+      checkResult.resultDescription should include("First mismatch was in ds where 2 row(s) had content: a=dupe, b=dupe, c=dupe, d=1")
+      checkResult.checkDescription shouldBe SimpleCheckDescription("Unordered dataset content matches")
+      checkResult.datasourceDescription shouldBe Some(DualDsDescription("df1", "df2"))
+    }
+
+    "return an error if the datasets have different content" in {
+      val df1 = DescribedDs(List(SampleData1("diff", "diff", "diff", 2)).toDS, "df1")
+      val df2 = DescribedDs(List(SampleData1("diff", "diff", "diff", 1)).toDS, "df2")
+      val dsPair = DescribedDsPair(df1, df2)
+      val checkResult = ArbDualDsCheck.dfsMatchUnordered.applyCheck(dsPair)
+      checkResult.qcType shouldBe QcType.ArbDualDsCheck
+      checkResult.status shouldBe CheckStatus.Error
+      checkResult.resultDescription should include(
+        "First mismatch was in dsToCompare where 1 row(s) had content: a=diff, b=diff, c=diff, d=1"
+      )
+      checkResult.checkDescription shouldBe SimpleCheckDescription("Unordered dataset content matches")
+      checkResult.datasourceDescription shouldBe Some(DualDsDescription("df1", "df2"))
+    }
+
+    "return success if the datasets have the same data, regardless of ordering" in {
+      val df1 = DescribedDs(List(SampleData1("same", "same", "same", 2), SampleData1("same", "same", "same", 1)).toDS, "df1")
+      val df2 = DescribedDs(List(SampleData1("same", "same", "same", 1), SampleData1("same", "same", "same", 2)).toDS, "df2")
+      val dsPair = DescribedDsPair(df1, df2)
+      val checkResult = ArbDualDsCheck.dfsMatchUnordered.applyCheck(dsPair)
+      checkResult.qcType shouldBe QcType.ArbDualDsCheck
+      checkResult.status shouldBe CheckStatus.Success
+      checkResult.resultDescription should include("Datasets matched")
+      checkResult.checkDescription shouldBe SimpleCheckDescription("Unordered dataset content matches")
+      checkResult.datasourceDescription shouldBe Some(DualDsDescription("df1", "df2"))
+    }
+
+    "return success if the datasets are both empty" in {
+      val df1 = DescribedDs(spark.emptyDataset[SampleData1], "df1")
+      val df2 = DescribedDs(spark.emptyDataset[SampleData1], "df2")
+      val dsPair = DescribedDsPair(df1, df2)
+      val checkResult = ArbDualDsCheck.dfsMatchUnordered.applyCheck(dsPair)
+      checkResult.qcType shouldBe QcType.ArbDualDsCheck
+      checkResult.status shouldBe CheckStatus.Success
+      checkResult.resultDescription should include("Datasets matched")
+      checkResult.checkDescription shouldBe SimpleCheckDescription("Unordered dataset content matches")
       checkResult.datasourceDescription shouldBe Some(DualDsDescription("df1", "df2"))
     }
   }
