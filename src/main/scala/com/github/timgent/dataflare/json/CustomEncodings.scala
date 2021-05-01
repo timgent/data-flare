2ac4ad87387fd978bed518099fe71cde74efba55
timgent <tim.gent@gmail.com>
2021-05-01 16:24:01 +0100

    Fix build

--- src/main/scala/com/github/timgent/dataflare/json/CustomEncodings.scala
+++ src/main/scala/com/github/timgent/dataflare/json/CustomEncodings.scala
@@ -14,6 +14,7 @@ import io.circe.Decoder.Result
 import io.circe.generic.semiauto.deriveEncoder
 import io.circe.syntax.EncoderOps
 import io.circe.{Decoder, Encoder, HCursor, Json}
+import cats.implicits.catsSyntaxEither
 
 import java.time.Instant
 
